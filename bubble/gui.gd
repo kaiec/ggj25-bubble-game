cd8949e40b7434659602cad0506eaf1892d5f6f7
Kai Eckert <hallo@kaiec.de>
2025-02-28 10:49:55 +0100

    GUI makeover

--- bubble/gui.gd
+++ bubble/gui.gd
@@ -28,14 +28,17 @@ func set_remaining_bubbles(amount : int):
 
 var animation_duration = 0.1
 func play_clicks_error():
+	var old_color = $Label.modulate
 	for i in range(3):
 		var tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel()
 		tween.tween_property($Label, "modulate", Color.RED, animation_duration)
+		tween.tween_property($Label2, "modulate", Color.RED, animation_duration)
 		tween.tween_property($Bubble, "modulate", Color.RED, animation_duration)
 		await tween.finished
 		tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel()
-		tween.tween_property($Label, "modulate", Color.WHITE, animation_duration)
-		tween.tween_property($Bubble, "modulate", Color.WHITE, animation_duration)
+		tween.tween_property($Label, "modulate", old_color, animation_duration)
+		tween.tween_property($Label2, "modulate", old_color, animation_duration)
+		tween.tween_property($Bubble, "modulate", old_color, animation_duration)
 		await tween.finished
 
 
